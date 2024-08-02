67f815879e7e77c41a2b4401c2db91c5da61dbb1
Jenna <88835851+itcodegirl@users.noreply.github.com>
2024-08-02 17:37:21 -0500

    Update src/index.js
    Updated the function showDate to ensure that the dropdown worked and alerts the date and time.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-//Europe/Paris time via moment-timezone
+
+function showDate(event) {
+	if (event.target.value.length > 0) {
+		let currentTime = moment()
+			.tz(event.target.value)
+			.format("dddd, MMMM D, YYYY h:mm A");
+
+		alert(`It is ${currentTime} in ${event.target.value}`);
+	}
+}
+
+let selectElement = document.querySelector("#city");
+selectElement.addEventListener("change", showDate);
